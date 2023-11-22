1fa293e8accb65a5e5342a6d2e2b60069e5c11e1
gronxb <gron1gh1@gmail.com>
2023-11-23 00:39:12 +0900

    chore(plugin-aws): init package

---
+++ plugins/aws/src/read.ts
@@ -0,0 +1,35 @@
+import type { HotUpdaterReadStrategy } from "@hot-updater/node";
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+
+export interface HotUpdaterAwsOptions {
+  s3Client: S3Client;
+  bucketName: string;
+  baseUrl: string;
+}
+
+export const aws = ({
+  baseUrl,
+  bucketName,
+  s3Client,
+}: HotUpdaterAwsOptions): HotUpdaterReadStrategy => {
+  return {
+    async getListObjects(prefix?: string) {
+      /**
+       * Uses ListObjectsV2Command to fetch a list of objects from an S3 bucket.
+       * Note: A single invocation of ListObjectsV2Command can retrieve a maximum of 1,000 objects.
+       */
+      const command = new ListObjectsV2Command({
+        Bucket: bucketName,
+        Prefix: prefix,
+      });
+
+      const data = await s3Client.send(command);
+      const files = data.Contents?.filter(({ Key }) => Key !== prefix).map(
+        (content) => [baseUrl, content.Key].join("/")
+      );
+      return files ?? [];
+    },
+  };
+};
+
+export { S3Client };
