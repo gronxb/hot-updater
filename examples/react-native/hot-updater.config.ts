07643fd784763140729fc5f8d2825f8120a3858d
gronxb <gron1gh1@gmail.com>
2024-06-23 14:39:55 +0900

    fix: config

--- examples/react-native/hot-updater.config.ts
+++ examples/react-native/hot-updater.config.ts
@@ -1,8 +1,17 @@
+import { uploadS3 } from "@hot-updater/aws";
 import { metro } from "@hot-updater/metro";
+
 import { defineConfig } from "hot-updater";
 
 export default defineConfig({
   updateServer: "",
   build: metro(),
-  deploy: () => {},
+  deploy: uploadS3({
+    region: "ap-northeast-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    bucketName: process.env.AWS_S3_BUCKET_NAME!,
+  }),
 });
