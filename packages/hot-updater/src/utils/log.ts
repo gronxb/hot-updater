1984e7a2aacfb64e98aba2b2c595f3a21afc6998
gronxb <gron1gh1@gmail.com>
2024-06-23 15:10:27 +0900

    feat: log

---
+++ packages/hot-updater/src/utils/log.ts
@@ -0,0 +1,14 @@
+import picocolors from "picocolors";
+
+export const log = {
+  success: (message: string | number | null | undefined) =>
+    console.log(picocolors.green(message)),
+  info: (message: string | number | null | undefined) =>
+    console.log(picocolors.blue(message)),
+  error: (message: string | number | null | undefined) =>
+    console.log(picocolors.red(message)),
+  warn: (message: string | number | null | undefined) =>
+    console.log(picocolors.yellow(message)),
+  debug: (message: string | number | null | undefined) =>
+    console.log(picocolors.gray(message)),
+};
