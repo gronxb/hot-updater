1448a7ac312b735d91c4482c9f51aa86812fcde2
Sungyu Kang <gron1gh1@gmail.com>
2024-08-17 18:25:13 +0900

    feat: remove comment

--- packages/hot-updater/src/commands/prune.tsx
+++ packages/hot-updater/src/commands/prune.tsx
@@ -1,50 +1,3 @@
-// import { getCwd } from "@/cwd.js";
-// import { loadConfig } from "@/utils/loadConfig.js";
-// import * as p from "@clack/prompts";
-// import type { Platform } from "@hot-updater/internal";
-
-// export interface PruneOptions {
-//   platform: Platform;
-// }
-
-// export const prune = async (options: PruneOptions) => {
-//   const s = p.spinner();
-
-//   const { deploy } = await loadConfig();
-
-//   const cwd = getCwd();
-
-//   const deployPlugin = deploy({
-//     cwd,
-//     spinner: s,
-//   });
-
-//   s.start("Checking existing updates");
-//   const updateSources = await deployPlugin.getUpdateJson();
-
-//   const activeSources = updateSources.filter((source) => source.enabled);
-//   const inactiveSources = updateSources.filter((source) => !source.enabled);
-
-//   if (inactiveSources.length === 0) {
-//     s.stop("No inactive versions found", -1);
-//     return;
-//   }
-
-//   s.message("Pruning updates");
-
-//   await deployPlugin.setUpdateJson(activeSources);
-//   await deployPlugin.commitUpdateJson();
-
-//   for (const source of inactiveSources) {
-//     const key = await deployPlugin.deleteBundle(
-//       options.platform,
-//       source.bundleVersion,
-//     );
-//     p.log.info(`deleting: ${key}`);
-//   }
-
-//   s.stop("Done");
-// };
 import { Banner } from "@/components/Banner.js";
 import { SelectInput } from "@/components/SelectInput.js";
 import { getCwd } from "@/cwd.js";
@@ -132,12 +85,12 @@ export default function Prune({ options }: Props) {
         }
         items={[
           {
-            label: "Yes",
-            value: true,
-          },
-          {
             label: "No",
             value: false,
+          },
+          {
+            label: "Yes",
+            value: true,
           },
         ]}
         onSelect={(item) => {
