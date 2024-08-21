ac764d066fbf3d07038f8b771fa2abd24eac1602
Sungyu Kang <gron1gh1@gmail.com>
2024-08-21 23:00:02 +0900

    fix: loading

--- packages/server/app/routes/_index.tsx
+++ packages/server/app/routes/_index.tsx
@@ -324,7 +324,7 @@ export default function Index() {
 
       <aside
         className={cn(
-          "fixed right-0 z-50 flex-col hidden min-w-64 h-full gap-4 p-4 sm:flex bg-muted ease-in-out duration-300",
+          "fixed right-0 z-50 flex-col hidden min-w-64 h-full gap-4 p-4 sm:flex bg-muted ease-in-out duration-300 shadow",
           isSidebarOpen ? "translate-x-0" : "translate-x-full",
         )}
         onClick={(e) => {
@@ -338,6 +338,29 @@ export default function Index() {
             setIsSidebarOpen(false);
           }}
         />
+
+        <div className="flex flex-col gap-1">
+          <div className="font-medium">Edit</div>
+          <div className="flex items-center gap-2">
+            <Checkbox id="forceUpdate">Force Update</Checkbox>
+            <label htmlFor="forceUpdate">Force Update</label>
+          </div>
+          <p className="text-xs text-gray-500">
+            When enabled, this update will require users to update before
+            continuing to use the application.
+          </p>
+
+          <div className="flex items-center gap-2">
+            <Checkbox id="enabled" defaultChecked>
+              Enabled
+            </Checkbox>
+            <label htmlFor="enabled">Enabled</label>
+          </div>
+          <p className="text-xs text-gray-500">
+            When disabled, this update will not be available to your users.
+          </p>
+          <Button>Save</Button>
+        </div>
       </aside>
     </div>
   );
