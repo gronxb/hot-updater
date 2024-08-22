9d3f0e37ae0bc27b4fec77e166b7252d9d69cb58
Sungyu Kang <gron1gh1@gmail.com>
2024-08-22 23:48:55 +0900

    fix: using hono

--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -1,5 +1,6 @@
 import devServer from "@hono/vite-dev-server";
 import { vitePlugin as remix } from "@remix-run/dev";
+import esbuild from "esbuild";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -16,6 +17,30 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         unstable_singleFetch: true,
+      },
+      serverBuildFile: "remix.js",
+      buildEnd: async () => {
+        await esbuild
+          .build({
+            alias: { "@": "./app" },
+            // The final file name
+            outfile: "build/server/index.js",
+            // Our server entry point
+            entryPoints: ["server/index.ts"],
+            // Dependencies that should not be bundled
+            // We import the remix build from "../build/server/remix.js", so no need to bundle it again aka remove all paths starting with
+            external: ["./build/server/*"],
+            platform: "node",
+            format: "esm",
+            // Don't include node_modules in the bundle
+            packages: "external",
+            bundle: true,
+            logLevel: "info",
+          })
+          .catch((error: unknown) => {
+            console.error(error);
+            process.exit(1);
+          });
       },
     }),
     tsconfigPaths(),
