798d24dc6d2dd42cdcd9835a42a70ce6180fa596
Sungyu Kang <gron1gh1@gmail.com>
2024-11-04 22:32:50 +0900

    fix: src and error

--- packages/hot-updater/src/commands/deploy.ts
+++ packages/hot-updater/src/commands/deploy.ts
@@ -17,83 +17,89 @@ export interface DeployOptions {
 export const deploy = async (options: DeployOptions) => {
   const s = spinner();
 
-  const config = await loadConfig();
-  if (!config) {
-    console.error("No config found. Please run `hot-updater init` first.");
-    process.exit(1);
-  }
-
-  intro("Please provide a description for the bundle.");
-  const description = await text({ message: "Description" });
-
-  const cwd = getCwd();
-  const targetVersion =
-    options.targetVersion ??
-    (await getDefaultTargetVersion(cwd, options.platform));
-
-  if (!targetVersion) {
-    throw new Error(
-      "Target version not found. Please provide a target version.",
-    );
-  }
-
-  s.start("Build in progress");
-
-  const { buildPath } = await config.build({
-    cwd,
-    platform: options.platform,
-  });
-  s.message("Checking existing updates...");
-
-  await createZip(buildPath, "build.zip");
-
-  const bundlePath = buildPath.concat(".zip");
-
-  const hash = await getFileHashFromFile(bundlePath);
-
-  const newBundleVersion = formatDate(new Date());
-
-  const deployPlugin = config.deploy({
-    cwd,
-  });
-
-  const updateSources = await deployPlugin.getUpdateSources();
-  const targetVersions = filterTargetVersion(
-    updateSources ?? [],
-    targetVersion,
-    options.platform,
-  );
-
-  // hash check
-  if (targetVersions.length > 0) {
-    const recentVersion = targetVersions[0];
-    const recentHash = recentVersion?.hash;
-
-    if (recentHash === hash) {
-      s.stop("The update already exists.", -1);
-      return;
+  try {
+    const config = await loadConfig();
+    if (!config) {
+      console.error("No config found. Please run `hot-updater init` first.");
+      process.exit(1);
     }
+
+    intro("Please provide a description for the bundle.");
+    const description = await text({ message: "Description" });
+
+    const cwd = getCwd();
+    const targetVersion =
+      options.targetVersion ??
+      (await getDefaultTargetVersion(cwd, options.platform));
+
+    if (!targetVersion) {
+      throw new Error(
+        "Target version not found. Please provide a target version.",
+      );
+    }
+
+    s.start("Build in progress");
+
+    const { buildPath } = await config.build({
+      cwd,
+      platform: options.platform,
+    });
+    s.message("Checking existing updates...");
+
+    await createZip(buildPath, "build.zip");
+
+    const bundlePath = buildPath.concat(".zip");
+
+    const hash = await getFileHashFromFile(bundlePath);
+
+    const newBundleVersion = formatDate(new Date());
+
+    const deployPlugin = config.deploy({
+      cwd,
+    });
+
+    const updateSources = await deployPlugin.getUpdateSources();
+    const targetVersions = filterTargetVersion(
+      updateSources ?? [],
+      targetVersion,
+      options.platform,
+    );
+
+    // hash check
+    if (targetVersions.length > 0) {
+      const recentVersion = targetVersions[0];
+      const recentHash = recentVersion?.hash;
+
+      if (recentHash === hash) {
+        s.stop("The update already exists.", -1);
+        return;
+      }
+    }
+
+    s.message("Uploading bundle...");
+    const { file } = await deployPlugin.uploadBundle(
+      options.platform,
+      newBundleVersion,
+      bundlePath,
+    );
+
+    await deployPlugin.appendUpdateSource({
+      forceUpdate: options.forceUpdate,
+      platform: options.platform,
+      file,
+      hash,
+      description: String(description),
+      targetVersion,
+      bundleVersion: newBundleVersion,
+      enabled: true,
+    });
+    await deployPlugin.commitUpdateSource();
+
+    await fs.rm(bundlePath);
+    s.stop("Uploading Success !", 0);
+  } catch (e) {
+    s.stop("Uploading Failed !", -1);
+    console.error(e);
+    process.exit(-1);
   }
-
-  s.message("Uploading bundle...");
-  const { file } = await deployPlugin.uploadBundle(
-    options.platform,
-    newBundleVersion,
-    bundlePath,
-  );
-
-  await deployPlugin.appendUpdateSource({
-    forceUpdate: options.forceUpdate,
-    platform: options.platform,
-    file,
-    hash,
-    description: String(description),
-    targetVersion,
-    bundleVersion: newBundleVersion,
-    enabled: true,
-  });
-  await deployPlugin.commitUpdateSource();
-
-  await fs.rm(bundlePath);
-  s.stop("Uploading Success !", 0);
 };
--- packages/hot-updater/src/commands/prune.ts
+++ packages/hot-updater/src/commands/prune.ts
@@ -9,42 +9,48 @@ export interface PruneOptions {
 export const prune = async (options: PruneOptions) => {
   const s = p.spinner();
 
-  const config = await loadConfig();
-  if (!config) {
-    console.error("No config found. Please run `hot-updater init` first.");
-    process.exit(1);
+  try {
+    const config = await loadConfig();
+    if (!config) {
+      console.error("No config found. Please run `hot-updater init` first.");
+      process.exit(1);
+    }
+
+    const cwd = getCwd();
+
+    const deployPlugin = config.deploy({
+      cwd,
+      // spinner: s,
+    });
+
+    s.start("Checking existing updates");
+    const updateSources = await deployPlugin.getUpdateSources();
+
+    const activeSources = updateSources.filter((source) => source.enabled);
+    const inactiveSources = updateSources.filter((source) => !source.enabled);
+
+    if (inactiveSources.length === 0) {
+      s.stop("No inactive versions found", -1);
+      return;
+    }
+
+    s.message("Pruning updates");
+
+    await deployPlugin.setUpdateSources(activeSources);
+    await deployPlugin.commitUpdateSource();
+
+    for (const source of inactiveSources) {
+      const key = await deployPlugin.deleteBundle(
+        options.platform,
+        source.bundleVersion,
+      );
+      p.log.info(`deleting: ${key}`);
+    }
+
+    s.stop("Done");
+  } catch (e) {
+    s.stop("Pruning Failed !", -1);
+    console.error(e);
+    process.exit(-1);
   }
-
-  const cwd = getCwd();
-
-  const deployPlugin = config.deploy({
-    cwd,
-    // spinner: s,
-  });
-
-  s.start("Checking existing updates");
-  const updateSources = await deployPlugin.getUpdateSources();
-
-  const activeSources = updateSources.filter((source) => source.enabled);
-  const inactiveSources = updateSources.filter((source) => !source.enabled);
-
-  if (inactiveSources.length === 0) {
-    s.stop("No inactive versions found", -1);
-    return;
-  }
-
-  s.message("Pruning updates");
-
-  await deployPlugin.setUpdateSources(activeSources);
-  await deployPlugin.commitUpdateSource();
-
-  for (const source of inactiveSources) {
-    const key = await deployPlugin.deleteBundle(
-      options.platform,
-      source.bundleVersion,
-    );
-    p.log.info(`deleting: ${key}`);
-  }
-
-  s.stop("Done");
 };
