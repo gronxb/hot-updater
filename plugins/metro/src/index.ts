f03c1f4cf5d4fd47ee944b874f08df6c17628961
Sungyu Kang <gron1gh1@gmail.com>
2024-11-05 00:39:13 +0900

    feat: metro using cli spawn

--- plugins/metro/src/index.ts
+++ plugins/metro/src/index.ts
@@ -1,94 +1,73 @@
+import { spawn } from "child_process";
 import path from "path";
 import type { BuildPluginArgs } from "@hot-updater/plugin-core";
 import fs from "fs/promises";
-import Metro from "metro";
-import type { InputConfigT } from "metro-config";
-import Server from "metro/src/Server";
+import { log } from "../../plugin-core/dist/index.cjs";
+
+interface RunBundleArgs {
+  cwd: string;
+  platform: string;
+  buildPath: string;
+}
+
+const runBundle = ({ cwd, platform, buildPath }: RunBundleArgs) => {
+  const reactNativePath = require.resolve("react-native");
+  const cliPath = path.resolve(reactNativePath, "..", "cli.js");
+
+  const bundleOutput = path.join(cwd, "build", `index.${platform}.bundle`);
+
+  const args = [
+    "bundle",
+    "--assets-dest",
+    buildPath,
+    "--bundle-output",
+    bundleOutput,
+    "--dev",
+    String(false),
+    "--entry-file",
+    "index.js",
+    "--platform",
+    String(platform),
+  ];
+
+  log.normal("\n");
+  const bundle = spawn(cliPath, args, { cwd });
+
+  return new Promise<void>((resolve, reject) => {
+    bundle.stdout?.on("data", (data: Buffer) => {
+      log.normal(data.toString().trim());
+    });
+
+    bundle.stderr?.on("data", (data: Buffer) => {
+      log.error(data.toString().trim());
+    });
+
+    bundle.on("close", (exitCode: number) => {
+      if (exitCode) {
+        reject(
+          new Error(
+            `"react-native bundle" command exited with code ${exitCode}.`,
+          ),
+        );
+      }
+
+      resolve();
+    });
+  });
+};
 
 export const metro =
-  (overrideConfig?: InputConfigT) =>
+  () =>
   async ({ cwd, platform }: BuildPluginArgs) => {
-    const config = await Metro.loadConfig({ cwd }, overrideConfig);
-
     const buildPath = path.join(cwd, "build");
 
     await fs.rm(buildPath, { recursive: true, force: true });
     await fs.mkdir(buildPath, { recursive: true });
 
-    const bundleOutput = path.join(cwd, "build", `index.${platform}.bundle`);
-    const outputs: string[] = [];
+    await runBundle({ cwd, platform, buildPath });
 
-    await Metro.runBuild(config, {
-      entry: "index.js",
-      output: {
-        build: async (server, options) => {
-          const bundleOptions = {
-            ...Server.DEFAULT_BUNDLE_OPTIONS,
-            ...options,
-          };
+    const outputs = await fs.readdir(buildPath, { recursive: true });
 
-          // copy assets
-          const assets = await server.getAssets({
-            ...bundleOptions,
-            bundleType: "bundle",
-          });
-
-          let copyTargetFiles: { from: string; to: string }[] = [];
-
-          switch (platform) {
-            case "ios": {
-              copyTargetFiles = assets
-                .flatMap((asset) => asset.files)
-                .map((file) => {
-                  const resolvedPath = file.replace(cwd, "");
-                  return {
-                    from: file,
-                    to: path.join(buildPath, "assets", resolvedPath),
-                  };
-                });
-              break;
-            }
-            case "android": {
-              copyTargetFiles = assets
-                .flatMap((asset) => asset.files)
-                .map((file) => {
-                  const resolvedPath = file
-                    .replace(`${cwd}/`, "")
-                    .replace(/\/|\\/g, "_");
-                  return {
-                    from: file,
-                    to: path.join(buildPath, "drawable-mdpi", resolvedPath),
-                  };
-                });
-              break;
-            }
-          }
-
-          await Promise.all(
-            copyTargetFiles.map(async ({ from, to }) => {
-              await fs.mkdir(path.dirname(to), { recursive: true });
-              await fs.copyFile(from, to);
-              outputs.push(to);
-            }),
-          );
-
-          return server.build(bundleOptions);
-        },
-        save: async ({ code, map }, options) => {
-          outputs.push(options.bundleOutput);
-          await fs.writeFile(options.bundleOutput, code);
-
-          if (options.sourcemapOutput) {
-            outputs.push(options.sourcemapOutput);
-            await fs.writeFile(options.sourcemapOutput, map);
-          }
-        },
-      },
-      out: bundleOutput,
-      platform,
-      minify: true,
-      sourceMap: true,
-    });
     return {
       buildPath,
       outputs,
