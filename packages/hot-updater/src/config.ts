9cef90fea6e9e3dc544612e3994442d6ed7005b5
Sungyu Kang <gron1gh1@gmail.com>
2024-11-04 23:54:47 +0900

    fix: async config

--- packages/hot-updater/src/config.ts
+++ packages/hot-updater/src/config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@hot-updater/plugin-core";
 
-export const defineConfig = (config: Config | (() => Config)): Config =>
-  typeof config === "function" ? config() : config;
+export const defineConfig = async (
+  config: Config | (() => Config) | (() => Promise<Config>),
+): Promise<Config> => (typeof config === "function" ? await config() : config);
