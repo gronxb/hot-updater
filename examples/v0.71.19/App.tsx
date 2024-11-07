19bf27c533c782f817fe21256b3c942972cc7508
Sungyu Kang <gron1gh1@gmail.com>
2024-11-07 23:36:23 +0900

    feat: support v0.71.19 (#26)
    * feat: init v0.71.19
    
    * fix: lock

---
+++ examples/v0.71.19/App.tsx
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { HotUpdater } from "@hot-updater/react-native";
+import type React from "react";
+import { useEffect, useState } from "react";
+import { Button, Image, SafeAreaView, Text } from "react-native";
+
+HotUpdater.init({
+  source: "https://gronxb.s3.ap-northeast-2.amazonaws.com/update.json",
+});
+
+function App(): React.JSX.Element {
+  const [version, setVersion] = useState<number | null>(null);
+
+  useEffect(() => {
+    HotUpdater.getBundleVersion().then((version) => {
+      setVersion(version);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Text
+        style={{
+          marginVertical: 20,
+          fontSize: 20,
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        Hot Updater 0
+      </Text>
+
+      <Text
+        style={{
+          marginVertical: 20,
+          fontSize: 20,
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        BundleVersion: {version}
+      </Text>
+
+      <Image
+        style={{
+          width: 100,
+          height: 100,
+        }}
+        // source={require("./src/logo.png")}
+        source={require("./src/test/_image.png")}
+      />
+
+      <Button title="Reload" onPress={() => HotUpdater.reload()} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
