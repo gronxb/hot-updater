da26769cddedcd84984ad4b35c371d71928e240a
Sungyu Kang <gron1gh1@gmail.com>
2024-11-13 01:57:28 +0900

    chore: support v0.76.1 new arch (#27)
    * chore: v76.1
    
    * chore: lock
    
    * fix: some test
    
    * fix: native code
    
    * fix: app.json
    
    * fix: dotenv

---
+++ examples/v0.76.1-new-arch/hot-updater.config.ts
@@ -0,0 +1,26 @@
+import { aws } from "@hot-updater/aws";
+import { metro } from "@hot-updater/metro";
+
+import { config } from "dotenv";
+import { defineConfig } from "hot-updater";
+
+config({
+  override: true,
+});
+
+export default defineConfig({
+  server: "",
+  secretKey: process.env.HOT_UPDATER_SECRET_KEY!,
+  build: metro(),
+  deploy: aws({
+    region: "ap-northeast-2",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    bucketName: process.env.AWS_S3_BUCKET_NAME!,
+  }),
+  // planning
+  // storage: s3Storage(), r2Storage(),
+  // deploy: supabase(),
+});
