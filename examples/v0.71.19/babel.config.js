9e7af32dc249864ef5a115374fb36beb0d94a58d
Sungyu Kang <gron1gh1@gmail.com>
2024-11-17 02:45:26 +0900

    refactor: migration bundle

--- examples/v0.71.19/babel.config.js
+++ examples/v0.71.19/babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['hot-updater/babel-plugin'],
 };
--- packages/hot-updater/src/plugins/babel.ts
+++ packages/hot-updater/src/plugins/babel.ts
@@ -5,7 +5,7 @@ import * as t from "@babel/types";
 const NIL_UUID = "00000000-0000-0000-0000-000000000000";
 
 export default function replaceHotUpdaterBundleId(): PluginObj {
-  const bundleId = process.env.HOT_UPDATER_BUNDLE_ID ?? NIL_UUID;
+  const bundleId = process.env["HOT_UPDATER_BUNDLE_ID"] ?? NIL_UUID;
 
   return {
     name: "replace-hot-updater-bundle-id",
