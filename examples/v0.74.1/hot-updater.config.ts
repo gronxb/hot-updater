04b61bcb5114edb157a4b31e7014f664523484cf
gronxb <gron1gh1@gmail.com>
2024-11-29 16:00:47 +0900

    fix: config

--- examples/v0.74.1/hot-updater.config.ts
+++ examples/v0.74.1/hot-updater.config.ts
@@ -1,4 +1,4 @@
-import { s3Database, s3Storage} from "@hot-updater/aws";
+import { s3Database, s3Storage } from "@hot-updater/aws";
 import { metro } from "@hot-updater/metro";
 
 import { config } from "dotenv";
@@ -10,20 +10,34 @@ config({
 
 export default defineConfig({
   build: metro(),
-  storage: s3Storage({
-    region: "ap-northeast-2",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  storage: s3Storage(
+    {
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      bucketName: process.env.AWS_S3_BUCKET_NAME!,
     },
-    bucketName: process.env.AWS_S3_BUCKET_NAME!,
-  }),
-  database: s3Database({
-    region: "ap-northeast-2",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    {
+      onStorageUploaded: async () => {
+        console.log("Storage Uploaded");
+      },
     },
-    bucketName: process.env.AWS_S3_BUCKET_NAME!,
-  }),
+  ),
+  database: s3Database(
+    {
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      bucketName: process.env.AWS_S3_BUCKET_NAME!,
+    },
+    {
+      onDatabaseUpdated: async () => {
+        console.log("Database Updated");
+      },
+    },
+  ),
 });
--- examples/v0.76.1-new-arch/hot-updater.config.ts
+++ examples/v0.76.1-new-arch/hot-updater.config.ts
@@ -1,4 +1,4 @@
-import { s3Database, s3Storage} from "@hot-updater/aws";
+import { s3Database, s3Storage } from "@hot-updater/aws";
 import { metro } from "@hot-updater/metro";
 
 import { config } from "dotenv";
@@ -10,28 +10,34 @@ config({
 
 export default defineConfig({
   build: metro(),
-  storage: s3Storage({
-    region: "ap-northeast-2",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  storage: s3Storage(
+    {
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      bucketName: process.env.AWS_S3_BUCKET_NAME!,
     },
-    bucketName: process.env.AWS_S3_BUCKET_NAME!,
-  }, {
-    onStorageUploaded: async (bundle) => {
-      console.log("Storage Uploaded", bundle);
+    {
+      onStorageUploaded: async () => {
+        console.log("Storage Uploaded");
+      },
     },
-  }),
-  database: s3Database({
-    region: "ap-northeast-2",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  ),
+  database: s3Database(
+    {
+      region: "ap-northeast-2",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      bucketName: process.env.AWS_S3_BUCKET_NAME!,
     },
-    bucketName: process.env.AWS_S3_BUCKET_NAME!,
-  }, {
-    onDatabaseUpdated: async (bundle) => {
-      console.log("Database Updated", bundle);
-    }
-  }),
+    {
+      onDatabaseUpdated: async () => {
+        console.log("Database Updated");
+      },
+    },
+  ),
 });
