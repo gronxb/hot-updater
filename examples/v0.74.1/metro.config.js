ba53df5a2635837673d18d0419237bbce233b29d
gronxb <gron1gh1@gmail.com>
2024-12-31 02:59:45 +0900

    fix: rnx-kit/metro-resolver-symlinks

--- examples/v0.74.1/metro.config.js
+++ examples/v0.74.1/metro.config.js
@@ -1,7 +1,8 @@
 const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
 module.exports = makeMetroConfig({
   resolver: {
-    unstable_enableSymlinks: true,
+    resolveRequest: MetroSymlinksResolver(),
   },
 });
