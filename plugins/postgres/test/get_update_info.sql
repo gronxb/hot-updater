b0dff12eb30064cca9b74b00f7903bd377e26823
gronxb <gron1gh1@gmail.com>
2024-12-30 19:26:04 +0900

    fix: test (8/18)

--- plugins/postgres/test/get_update_info.sql
+++ plugins/postgres/test/get_update_info.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION get_update_info (
     current_platform   platforms,
     current_bundle_id  uuid,
@@ -14,12 +13,15 @@ RETURNS TABLE (
 LANGUAGE plpgsql
 AS
 $$
+DECLARE
+    NIL_UUID CONSTANT uuid := '00000000-0000-0000-0000-000000000000';
 BEGIN
     RETURN QUERY
     WITH rollback_candidate AS (
         SELECT
             b.id,
-            b.force_update,
+            -- status가 'ROLLBACK'이면 force_update는 무조건 TRUE
+            TRUE AS force_update,
             b.file_url,
             b.file_hash,
             'ROLLBACK' AS status
@@ -43,14 +45,34 @@ BEGIN
           AND semver_satisfies(b.target_version, current_app_version)
         ORDER BY b.id DESC
         LIMIT 1
+    ),
+    final_result AS (
+        SELECT *
+        FROM rollback_candidate
+
+        UNION ALL
+
+        SELECT *
+        FROM update_candidate
+        WHERE NOT EXISTS (SELECT 1 FROM rollback_candidate)
     )
     SELECT *
-    FROM rollback_candidate
+    FROM final_result
 
     UNION ALL
+    /*
+      최종 결과가 0개이고, current_bundle_id != NIL_UUID일 때
+      fallback row를 1개 추가.
+      이 fallback row도 ROLLBACK 이므로 forceUpdate = TRUE.
+    */
+    SELECT
+        NIL_UUID      AS id,
+        TRUE          AS force_update,  -- 무조건 TRUE
+        NULL          AS file_url,
+        NULL          AS file_hash,
+        'ROLLBACK'    AS status
+    WHERE (SELECT COUNT(*) FROM final_result) = 0
+      AND current_bundle_id != NIL_UUID;
 
-    SELECT *
-    FROM update_candidate
-    WHERE NOT EXISTS (SELECT 1 FROM rollback_candidate);
 END;
 $$;
