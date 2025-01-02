44ee131dc69989e390afae6a8b96c181a8349d51
gronxb <gron1gh1@gmail.com>
2025-01-03 02:44:28 +0900

    feat: supabase plugins

---
+++ plugins/supabase/sql/bundles.sql
@@ -0,0 +1,15 @@
+CREATE TYPE platforms AS ENUM ('ios', 'android');
+
+CREATE TABLE bundles (
+    id uuid PRIMARY KEY,
+    platform platforms NOT NULL,
+    target_app_version text NOT NULL,
+    force_update boolean NOT NULL,
+    enabled boolean NOT NULL,
+    file_url text NOT NULL,
+    file_hash text NOT NULL,
+    git_commit_hash text,
+    message text
+);
+
+CREATE INDEX bundles_target_app_version_idx ON bundles(target_app_version);
