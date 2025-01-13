3cd03a1845849ea5e1d63e94f3fa2ac94a2ade2d
Sungyu Kang <gron1gh1@gmail.com>
2025-01-13 09:57:20 +0900

    feat(react-native): HotUpdater.wrap Component (#41)
    * feat(react-native): HotUpdater.wrap Component
    
    * fix: import

--- examples/v0.71.19/App.tsx
+++ examples/v0.71.19/App.tsx
@@ -5,27 +5,13 @@
  * @format
  */
 
-import { HotUpdater, useHotUpdaterStore } from "@hot-updater/react-native";
+import { HotUpdater } from "@hot-updater/react-native";
 import type React from "react";
 import { useEffect, useState } from "react";
 import { Button, Image, SafeAreaView, Text } from "react-native";
 
-HotUpdater.init({
-  source: "https://gronxb.s3.ap-northeast-2.amazonaws.com/update.json",
-});
-
-function extractTimestampFromUUIDv7(uuid: string) {
-  const timestampHex = uuid.split("-").join("").slice(0, 12);
-
-  const timestamp = Number.parseInt(timestampHex, 16);
-
-  return timestamp;
-}
-
 function App(): React.JSX.Element {
   const [bundleId, setBundleId] = useState<string | null>(null);
-
-  const { progress } = useHotUpdaterStore();
 
   useEffect(() => {
     const bundleId = HotUpdater.getBundleId();
@@ -37,11 +23,7 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView>
-      <Text>Progress: {Math.round(progress * 100)}%</Text>
-      <Text>Babel {HotUpdater.HOT_UPDATER_BUNDLE_ID}</Text>
-      <Text>
-        Babel {extractTimestampFromUUIDv7(HotUpdater.HOT_UPDATER_BUNDLE_ID)}
-      </Text>
+      <Text>Babel {HotUpdater.getBundleId()}</Text>
       <Text
         style={{
           marginVertical: 20,
@@ -89,4 +71,7 @@ function App(): React.JSX.Element {
   );
 }
 
-export default App;
+export default HotUpdater.wrap({
+  source: "https://inodtkixxqmthzanatwg.supabase.co/functions/v1/update-server",
+  fallbackComponent: ({ progress }) => <Text>{progress}</Text>,
+})(App);
