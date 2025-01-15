3a91742b5ad9bb9e7455b3e9f42d79d4b677b693
gronxb <gron1gh1@gmail.com>
2025-01-16 07:51:46 +0900

    feat: init command

--- packages/hot-updater/src/components/banner.tsx
+++ packages/hot-updater/src/components/banner.tsx
@@ -2,13 +2,15 @@ import { version } from "@/packageJson";
 import boxen from "boxen";
 import picocolors from "picocolors";
 
+export const link = (url: string) => {
+  return picocolors.green(picocolors.underline(url));
+};
+
 export const banner = boxen(
   [
     `${picocolors.bold("Hot Updater - React Native OTA Solution")} v${version}`,
     "",
-    `Github: ${picocolors.green(
-      picocolors.underline("https://github.com/gronxb/hot-updater"),
-    )}`,
+    `Github: ${link("https://github.com/gronxb/hot-updater")}`,
     "Give a ⭐️ if you like it!",
   ].join("\n"),
   {
