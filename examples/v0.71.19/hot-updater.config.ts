97189efd9cbebcdf6e299a2196b0e8e32b91be29
gronxb <gron1gh1@gmail.com>
2025-01-17 01:34:39 +0900

    fix: arch

--- examples/v0.71.19/hot-updater.config.ts
+++ examples/v0.71.19/hot-updater.config.ts
@@ -1,43 +1,17 @@
-import { s3Database, s3Storage } from "@hot-updater/aws";
 import { metro } from "@hot-updater/metro";
-
-import { config } from "dotenv";
+import { supabaseDatabase, supabaseStorage } from "@hot-updater/supabase";
 import { defineConfig } from "hot-updater";
-
-config({
-  override: true,
-});
+import "dotenv/config";
 
 export default defineConfig({
   build: metro(),
-  storage: s3Storage(
-    {
-      region: "ap-northeast-2",
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      },
-      bucketName: process.env.AWS_S3_BUCKET_NAME!,
-    },
-    {
-      onStorageUploaded: async () => {
-        console.log("Storage Uploaded");
-      },
-    },
-  ),
-  database: s3Database(
-    {
-      region: "ap-northeast-2",
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      },
-      bucketName: process.env.AWS_S3_BUCKET_NAME!,
-    },
-    {
-      onDatabaseUpdated: async () => {
-        console.log("Database Updated");
-      },
-    },
-  ),
+  storage: supabaseStorage({
+    supabaseUrl: process.env.HOT_UPDATER_SUPABASE_URL!,
+    supabaseAnonKey: process.env.HOT_UPDATER_SUPABASE_ANON_KEY!,
+    bucketName: process.env.HOT_UPDATER_SUPABASE_BUCKET_NAME!,
+  }),
+  database: supabaseDatabase({
+    supabaseUrl: process.env.HOT_UPDATER_SUPABASE_URL!,
+    supabaseAnonKey: process.env.HOT_UPDATER_SUPABASE_ANON_KEY!,
+  }),
 });
