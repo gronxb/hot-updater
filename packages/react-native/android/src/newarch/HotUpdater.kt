a3feb7c5b7312fc0274b9ffbcf55ab7eb510b763
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-01-17 02:52:19 +0900

    [#47] Add getJSBundleFile into HotUpdater

--- packages/react-native/android/src/newarch/HotUpdater.kt
+++ packages/react-native/android/src/newarch/HotUpdater.kt
@@ -1,10 +1,12 @@
 package com.hotupdater
 
+import android.content.Context
 import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import java.io.File
 
 class HotUpdater : BaseReactPackage() {
     override fun getModule(
@@ -32,4 +34,34 @@ class HotUpdater : BaseReactPackage() {
                 )
             moduleInfos
         }
+
+    companion object {
+        @JvmStatic
+        fun getJSBundleFile(context: Context): String? {
+            val sharedPreferences = context.getSharedPreferences(
+                "HotUpdaterPrefs",
+                Context.MODE_PRIVATE
+            )
+            val urlString = sharedPreferences.getString(
+                "HotUpdaterBundleURL",
+                null
+            )
+            if (urlString.isNullOrEmpty()) {
+                return "assets://index.android.bundle"
+            }
+
+            val file = File(urlString)
+            if (!file.exists()) {
+                sharedPreferences.edit()
+                    .putString(
+                        "HotUpdaterBundleURL",
+                        null
+                    )
+                    .apply()
+                return "assets://index.android.bundle"
+            }
+
+            return urlString
+        }
+    }
 }
--- packages/react-native/android/src/oldarch/HotUpdater.kt
+++ packages/react-native/android/src/oldarch/HotUpdater.kt
@@ -1,13 +1,49 @@
 package com.hotupdater
 
+import android.content.Context
 import android.view.View
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
+import java.io.File
 
 class HotUpdater : ReactPackage {
-    override fun createViewManagers(context: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+    override fun createViewManagers(
+        context: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 
-    override fun createNativeModules(context: ReactApplicationContext) = listOf(HotUpdaterModule(context = context))
+    override fun createNativeModules(
+        context: ReactApplicationContext
+    ) = listOf(HotUpdaterModule(context = context))
+
+    companion object {
+        @JvmStatic
+        fun getJSBundleFile(context: Context): String? {
+            val sharedPreferences = context.getSharedPreferences(
+                "HotUpdaterPrefs",
+                Context.MODE_PRIVATE
+            )
+            val urlString = sharedPreferences.getString(
+                "HotUpdaterBundleURL",
+                null
+            )
+            if (urlString.isNullOrEmpty()) {
+                return "assets://index.android.bundle"
+            }
+
+            val file = File(urlString)
+            if (!file.exists()) {
+                sharedPreferences.edit()
+                    .putString(
+                        "HotUpdaterBundleURL",
+                        null
+                    )
+                    .apply()
+                return "assets://index.android.bundle"
+            }
+
+            return urlString
+        }
+    }
 }
