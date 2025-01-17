975375b298e5efdfc13ed2a21d1ebe73dfa0158e
gronxb <gron1gh1@gmail.com>
2025-01-17 18:29:16 +0900

    feat: reloadOnForceUpdate

--- packages/react-native/src/runUpdateProcess.ts
+++ packages/react-native/src/runUpdateProcess.ts
@@ -1,5 +1,5 @@
 import { type CheckForUpdateConfig, checkForUpdate } from "./checkUpdate";
-import { updateBundle } from "./native";
+import { reload, updateBundle } from "./native";
 
 export type RunUpdateProcessResponse =
   | {
@@ -11,8 +11,17 @@ export type RunUpdateProcessResponse =
       status: "UP_TO_DATE";
     };
 
+export interface RunUpdateProcessConfig extends CheckForUpdateConfig {
+  /**
+   * If `true`, the app will be reloaded when the downloaded bundle is a force update.
+   * If `false`, isForceUpdate will be returned as true but the app won't reload.
+   * @default false
+   */
+  reloadOnForceUpdate?: boolean;
+}
+
 export const runUpdateProcess = async (
-  config: CheckForUpdateConfig,
+  config: RunUpdateProcessConfig,
 ): Promise<RunUpdateProcessResponse> => {
   const updateInfo = await checkForUpdate(config);
   if (!updateInfo) {
@@ -22,6 +31,9 @@ export const runUpdateProcess = async (
   }
 
   await updateBundle(updateInfo.id, updateInfo.fileUrl);
+  if (updateInfo.forceUpdate && config.reloadOnForceUpdate) {
+    reload();
+  }
   return {
     status: updateInfo.status,
     isForceUpdate: updateInfo.forceUpdate,
