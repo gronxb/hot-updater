5aaeabc6bfc5d77f512c1d9bdd28af2eda12a31d
gronxb <gron1gh1@gmail.com>
2025-01-23 21:52:24 +0900

    chore: example using react-native-env

--- examples/v0.74.1/babel.config.js
+++ examples/v0.74.1/babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['hot-updater/babel-plugin'],
+  plugins: [
+    'hot-updater/babel-plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        allowlist: ['HOT_UPDATER_SUPABASE_URL'],
+        path: '.env',
+      },
+    ],
+  ],
 };
 
