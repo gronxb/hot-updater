d0ed1d91e80e8f0352dfabb05e5ed9404b9f9307
Coreight98 <maxcha98@naver.com>
2025-02-10 00:02:35 +0900

    feat: mock database plugin

---
+++ plugins/mock/src/mockDatabase.ts
@@ -0,0 +1,42 @@
+import type {
+    BasePluginArgs,
+    Bundle,
+    DatabasePlugin,
+    DatabasePluginHooks
+  } from '@hot-updater/plugin-core';
+  
+  export interface MockDatabaseConfig {
+    initialBundles?: Bundle[];
+  }
+  
+  export const mockDatabase =
+  (config: MockDatabaseConfig, hooks?: DatabasePluginHooks) => (_: BasePluginArgs): DatabasePlugin => {
+    let bundles: Bundle[] = config.initialBundles ?? [];
+  
+    return {
+      name: 'mockDatabase',
+      async commitBundle () {
+        hooks?.onDatabaseUpdated?.();
+      },
+      async updateBundle (targetBundleId: string, newBundle: Partial<Bundle>) {
+        const targetIndex = bundles.findIndex(u => u.id === targetBundleId);
+        if (targetIndex === -1) {
+          throw new Error('target bundle version not found');
+        }
+        Object.assign(bundles[targetIndex], newBundle);
+      },
+      async appendBundle (inputBundle: Bundle) {
+        bundles.unshift(inputBundle);
+      },
+      async setBundles (newBundles: Bundle[]) {
+        bundles = newBundles;
+      },
+      async getBundleById (bundleId: string) {
+        return bundles.find(b => b.id === bundleId) ?? null;
+      },
+      async getBundles () {
+        return bundles.sort((a, b) => a.id.localeCompare(b.id));
+      },
+    };
+  };
+  
