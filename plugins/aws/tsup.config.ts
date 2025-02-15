2766b7f7f6c552ec4374737e9e62b6ab6d5351cf
gronxb <gron1gh1@gmail.com>
2025-02-16 03:39:03 +0900

    fix: build

--- plugins/aws/tsup.config.ts
+++ plugins/aws/tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/sdk.ts", "lambda/index.ts"],
+  entry: ["src/index.ts", "sdk/index.ts", "lambda/index.ts"],
   format: ["esm", "cjs"],
+  outDir: "dist",
   dts: true,
   banner: {
     js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
