6a8f3a5276865d7381957cb2bcd3e98a99917d94
Gun Yu <yugun000@gmail.com>
2025-02-17 22:29:09 +0900

    fix: bundle Lambda@Edge dependencies into a single file (#113)
    * fix: bundle Lambda@Edge dependencies into a single file
    
     - minor Lambda fix
     - remove mistakenly committed file
    
    * Update plugins/aws/lambda/index.ts
    
    Co-authored-by: Sungyu Kang <gron1gh1@gmail.com>
    
    ---------
    
    Co-authored-by: Sungyu Kang <gron1gh1@gmail.com>

--- plugins/aws/tsup.config.ts
+++ plugins/aws/tsup.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts", "sdk/index.ts", "lambda/index.ts"],
+export default defineConfig([{
+  entry: ["src/index.ts", "src/sdk.ts"],
   format: ["esm", "cjs"],
   outDir: "dist",
   dts: true,
   banner: {
     js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
   },
-});
+}, {
+  entry: ["lambda/index.ts"],
+  format: ["cjs"],
+  outDir: "dist/lambda",
+  sourcemap: false,
+  splitting: false,
+  clean: true,
+  dts: false,
+  external: ["aws-sdk"],
+  noExternal: ["@hot-updater/js", "@hot-updater/core"]
+}]);
