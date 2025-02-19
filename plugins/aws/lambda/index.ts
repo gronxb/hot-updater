8ef5f4b43028e4728892f5c4a3ace068bc3f6353
rjwu95 <yugun000@gmail.com>
2025-02-19 12:03:49 +0900

    fix: return null instead of throwing an error when target-app-versions.json is not yet created

--- plugins/aws/lambda/index.ts
+++ plugins/aws/lambda/index.ts
@@ -2,11 +2,7 @@ import type { Bundle } from "@hot-updater/core";
 import { filterCompatibleAppVersions, getUpdateInfo } from "@hot-updater/js";
 import type { CloudFrontRequestHandler } from "aws-lambda";
 
-export const handler: CloudFrontRequestHandler = async (
-  event,
-  context,
-  callback,
-) => {
+export const handler: CloudFrontRequestHandler = async (event, context, callback) => {
   const request = event.Records[0].cf.request;
   const headers = request.headers;
 
@@ -17,9 +13,7 @@ export const handler: CloudFrontRequestHandler = async (
   const distributionDomain = headers["host"][0]?.value;
 
   const bundleId = headers["x-bundle-id"]?.[0]?.value as string;
-  const appPlatform = headers["x-app-platform"]?.[0]?.value as
-    | "ios"
-    | "android";
+  const appPlatform = headers["x-app-platform"]?.[0]?.value as "ios" | "android";
   const appVersion = headers["x-app-version"]?.[0]?.value as string;
 
   if (!bundleId || !appPlatform || !appVersion) {
@@ -38,11 +32,13 @@ export const handler: CloudFrontRequestHandler = async (
     method: "GET",
   });
   if (!targetAppVersionListResponse.ok) {
+    // target-app-versions.json has not been created yet
     callback(null, {
-      status: "404",
-      body: JSON.stringify({
-        error: `Failed to fetch ${appPlatform}/target-app-versions.json`,
-      }),
+      status: "200",
+      headers: {
+        "Content-Type": [{ key: "Content-Type", value: "application/json" }],
+      },
+      body: JSON.stringify(null),
     });
     return;
   }
