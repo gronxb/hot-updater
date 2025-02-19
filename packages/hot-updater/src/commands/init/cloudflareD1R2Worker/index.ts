ea693f8f10ea0877642f04f62734c25d3dee927d
gronxb <gron1gh1@gmail.com>
2025-02-20 03:13:40 +0900

    chore: update hermes

--- packages/hot-updater/src/commands/init/cloudflareD1R2Worker/index.ts
+++ packages/hot-updater/src/commands/init/cloudflareD1R2Worker/index.ts
@@ -15,7 +15,7 @@ import { defineConfig } from "hot-updater";
 import "dotenv/config";
 
 export default defineConfig({
-  build: metro(),
+  build: metro({ enableHermes: true }),
   storage: r2Storage({
     bucketName: process.env.HOT_UPDATER_CLOUDFLARE_R2_BUCKET_NAME!,
     accountId: process.env.HOT_UPDATER_CLOUDFLARE_ACCOUNT_ID!,
