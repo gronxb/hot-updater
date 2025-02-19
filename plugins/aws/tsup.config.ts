a4e0dd43edec65e510786a61d8aa09a1f3550df3
rjwu95 <yugun000@gmail.com>
2025-02-19 13:14:32 +0900

    run code formatter

--- plugins/aws/tsup.config.ts
+++ plugins/aws/tsup.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([{
-  entry: ["src/index.ts", "sdk/index.ts"],
-  format: ["esm", "cjs"],
-  outDir: "dist",
-  dts: true,
-  banner: {
-    js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+export default defineConfig([
+  {
+    entry: ["src/index.ts", "sdk/index.ts"],
+    format: ["esm", "cjs"],
+    outDir: "dist",
+    dts: true,
+    banner: {
+      js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+    },
   },
-},{
-  entry: ["lambda/index.ts"],
-  format: ["cjs"],
-  outDir: "dist/lambda",
-}]);
+  {
+    entry: ["lambda/index.ts"],
+    format: ["cjs"],
+    outDir: "dist/lambda",
+  },
+]);
