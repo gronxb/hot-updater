5460632ff3405b2d74161efcedfd47715e858700
Coreight98 <maxcha98@naver.com>
2025-02-23 15:44:25 +0900

    fix: Update the relative path in the prod env

--- packages/console/src-server/index.ts
+++ packages/console/src-server/index.ts
@@ -1,14 +1,26 @@
+import path from "path";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 import { rpc } from "./rpc";
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const relativePathToScript = import.meta.env.PROD
+  ? path.relative(process.cwd(), __dirname)
+  : "/";
+
 const app = new Hono()
   .get("/ping", (c) => c.text("pong"))
   .route("/rpc", rpc)
+  .use(
+    "/assets/*",
+    serveStatic({
+      root: relativePathToScript,
+    }),
+  )
   .get(
     "/",
     serveStatic({
-      root: "/",
+      root: relativePathToScript,
       path: "index.html",
     }),
   );
