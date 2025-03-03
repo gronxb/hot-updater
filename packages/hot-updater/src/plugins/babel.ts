dca0d974111533020ff06d508b6f38ab2745f0d2
gronxb <gron1gh1@gmail.com>
2025-03-04 04:00:30 +0900

    fix(babel): initial bundles

--- packages/hot-updater/src/plugins/babel.ts
+++ packages/hot-updater/src/plugins/babel.ts
@@ -3,31 +3,11 @@ import path from "path";
 import type { PluginObj } from "@babel/core";
 import type { NodePath } from "@babel/traverse";
 import * as t from "@babel/types";
+import picocolors from "picocolors";
 import { uuidv7 } from "uuidv7";
 
 export default function replaceHotUpdaterBundleId(): PluginObj {
-  const buildOutDir = process.env["BUILD_OUT_DIR"];
-
-  if (!buildOutDir) {
-    return {
-      name: "replace-hot-updater-bundle-id",
-      visitor: {
-        MemberExpression(path: NodePath<t.MemberExpression>) {
-          if (
-            t.isIdentifier(path.node.object, { name: "HotUpdater" }) &&
-            t.isIdentifier(path.node.property, {
-              name: "HOT_UPDATER_BUNDLE_ID",
-            })
-          ) {
-            path.replaceWith(
-              t.stringLiteral("00000000-0000-0000-0000-000000000000"),
-            );
-          }
-        },
-      },
-    };
-  }
-
+  const buildOutDir = process.env?.["BUILD_OUT_DIR"] ?? process.cwd();
   const bundleIdPath = path.join(buildOutDir, "BUNDLE_ID");
 
   let bundleId = uuidv7();
@@ -36,6 +16,9 @@ export default function replaceHotUpdaterBundleId(): PluginObj {
     bundleId = fs.readFileSync(bundleIdPath, "utf-8");
   } else {
     fs.writeFileSync(bundleIdPath, bundleId);
+    console.log(
+      picocolors.green(`[HotUpdater] Generated bundle ID: ${bundleId}`),
+    );
   }
 
   return {
