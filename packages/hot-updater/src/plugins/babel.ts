3628b43cfb7752781e184ffde26a30d42d30fe52
gronxb <gron1gh1@gmail.com>
2025-03-05 09:39:48 +0900

    fix: rollback

--- packages/hot-updater/src/plugins/babel.ts
+++ packages/hot-updater/src/plugins/babel.ts
@@ -7,7 +7,28 @@ import picocolors from "picocolors";
 import { uuidv7 } from "uuidv7";
 
 export default function replaceHotUpdaterBundleId(): PluginObj {
-  const buildOutDir = process.env?.["BUILD_OUT_DIR"] ?? process.cwd();
+  const buildOutDir = process.env["BUILD_OUT_DIR"];
+
+  if (!buildOutDir) {
+    return {
+      name: "replace-hot-updater-bundle-id",
+      visitor: {
+        MemberExpression(path: NodePath<t.MemberExpression>) {
+          if (
+            t.isIdentifier(path.node.object, { name: "HotUpdater" }) &&
+            t.isIdentifier(path.node.property, {
+              name: "HOT_UPDATER_BUNDLE_ID",
+            })
+          ) {
+            path.replaceWith(
+              t.stringLiteral("00000000-0000-0000-0000-000000000000"),
+            );
+          }
+        },
+      },
+    };
+  }
+
   const bundleIdPath = path.join(buildOutDir, "BUNDLE_ID");
 
   let bundleId = uuidv7();
