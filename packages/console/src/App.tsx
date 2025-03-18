133431a671ebe41dac3d896df77e9b9e003ccaa6
gronxb <gron1gh1@gmail.com>
2025-03-18 10:07:54 +0900

    feat(console): icon colorful

--- packages/console/src/App.tsx
+++ packages/console/src/App.tsx
@@ -1,10 +1,11 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Route, Router } from "@solidjs/router";
 import "./App.css";
-import { SplashScreen } from "@/components/spash-screen";
 import Layout from "@/components/ui/layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { Suspense } from "solid-js";
+import { SplashScreen } from "./components/spash-screen";
+import { Toaster } from "./components/ui/toast";
 import Home from "./routes";
 
 const queryClient = new QueryClient();
@@ -25,6 +26,8 @@ export default function App() {
         <Route path="/" component={Home} />
         <Route path="/:bundleId" component={Home} />
       </Router>
+
+      <Toaster />
     </QueryClientProvider>
   );
 }
