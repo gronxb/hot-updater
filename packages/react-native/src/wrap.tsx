c1b505a4102b42629ad4fa244fa5337076756619
Filip Hájek <flp.hajek@gmail.com>
2025-03-22 04:58:57 +0100

    refactor(react-native): simplify import statement in wrap.tsx (#199)
    * refactor(react-native): simplify import statement in wrap.tsx
    
    * fix: biome
    
    ---------
    
    Co-authored-by: gronxb <gron1gh1@gmail.com>

--- packages/react-native/src/wrap.tsx
+++ packages/react-native/src/wrap.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import { useEffect, useLayoutEffect, useState } from "react";
 import { type CheckForUpdateConfig, checkForUpdate } from "./checkForUpdate";
 import { HotUpdaterError } from "./error";
