b72e6e4ea5a70298fd1cf9ecff8a47adf4d0fce8
Sungyu Kang <gron1gh1@gmail.com>
2025-03-28 01:45:06 +0900

    fix(metro): ensure `bundle.map` is generated correctly when `enableHermes` is set to true (#219)
    * fix(metro): with js source map when enabledHermes true
    
    * feat: createZipTargetFiles
    
    * fix: deploy

--- plugins/plugin-core/src/createZip.ts
+++ plugins/plugin-core/src/createZip.ts
@@ -2,6 +2,61 @@ import path from "path";
 import fs from "fs/promises";
 import JSZip from "jszip";
 
+export const createZipTargetFiles = async ({
+  outfile,
+  targetFiles,
+}: {
+  targetFiles: { path: string; name: string }[];
+  outfile: string;
+}) => {
+  const zip = new JSZip();
+  await fs.rm(outfile, { force: true });
+
+  async function addFiles(dir: string, zipFolder: JSZip) {
+    const files = await fs.readdir(dir);
+    files.sort();
+
+    for (const file of files) {
+      const fullPath = path.join(dir, file);
+      const stats = await fs.stat(fullPath);
+
+      if (stats.isDirectory()) {
+        const folder = zipFolder.folder(file);
+        if (!folder) continue;
+        await addFiles(fullPath, folder);
+      } else {
+        const data = await fs.readFile(fullPath);
+        zipFolder.file(file, data);
+      }
+    }
+  }
+
+  for (const target of targetFiles) {
+    const stats = await fs.stat(target.path);
+    if (stats.isDirectory()) {
+      const folder = zip.folder(target.name);
+      if (folder) {
+        await addFiles(target.path, folder);
+      }
+    } else {
+      const data = await fs.readFile(target.path);
+      zip.file(target.name, data);
+    }
+  }
+
+  const content = await zip.generateAsync({
+    type: "nodebuffer",
+    compression: "DEFLATE",
+    compressionOptions: {
+      level: 9,
+    },
+    platform: "UNIX",
+  });
+
+  await fs.writeFile(outfile, content);
+  return outfile;
+};
+
 export const createZip = async ({
   outfile,
   targetDir,
