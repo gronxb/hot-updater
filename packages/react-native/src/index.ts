cd5a01ef93344f8ca238ff880813c6fa420f7df1
gronxb <gron1gh1@gmail.com>
2025-03-27 22:01:44 +0900

    feat(native): notifyAppReady

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -5,6 +5,7 @@ import {
   getBundleId,
   getChannel,
   getMinBundleId,
+  notifyAppReady,
   reload,
   updateBundle,
 } from "./native";
@@ -188,4 +189,18 @@ export const HotUpdater = {
    * ```
    */
   updateBundle,
+  /**
+   * Marks the update as successful to prevent rollback.
+   *
+   * When you're manually handling app updates without using `HotUpdater.wrap` or `HotUpdater.runUpdateProcess`,
+   * you **must** call this method after a successful update. If you skip this step,
+   * Hot Updater will assume the update failed and will revert to the previous version
+   * the next time the app restarts.
+   *
+   * @example
+   * ```ts
+   * await HotUpdater.notifyAppReady();
+   * ```
+   */
+  notifyAppReady,
 };
