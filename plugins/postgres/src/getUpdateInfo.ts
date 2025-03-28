59843823fe903fe9ff3e40943e2f8dfce62e54a4
kyoungminkim <kyoungmin@getmiso.com>
2025-03-28 11:08:06 +0900

    use minify

--- plugins/postgres/src/getUpdateInfo.ts
+++ plugins/postgres/src/getUpdateInfo.ts
@@ -6,6 +6,7 @@ import {
 } from "@hot-updater/core";
 import camelcaseKeys from "camelcase-keys";
 import pg from "pg";
+import minify from "pg-minify";
 
 export const getUpdateInfo = async (
   pool: pg.Pool,
@@ -17,36 +18,45 @@ export const getUpdateInfo = async (
     channel = "production",
   }: GetBundlesArgs,
 ) => {
+  const sqlGetTargetAppVersionList = minify(`
+    SELECT target_app_version 
+    FROM get_target_app_version_list($1, $2);
+  `);
+
   const { rows: appVersionList } = await pool.query<{
     target_app_version: string;
-  }>(
-    `
-       SELECT target_app_version FROM get_target_app_version_list('${platform}', '${minBundleId}');
-       `,
-  );
+  }>(sqlGetTargetAppVersionList, [platform, minBundleId]);
 
   const targetAppVersionList = filterCompatibleAppVersions(
     appVersionList?.map((group) => group.target_app_version) ?? [],
     appVersion,
   );
 
+  const sqlGetUpdateInfo = minify(`
+    SELECT *
+    FROM get_update_info(
+      $1, -- platform
+      $2, -- appVersion
+      $3, -- bundleId
+      $4, -- minBundleId (nullable)
+      $5, -- channel
+      $6 -- targetAppVersionList (text array)
+    );
+  `);
+
   const result = await pool.query<{
     id: string;
     should_force_update: boolean;
     message: string;
     status: string;
-  }>(
-    `
-       SELECT * FROM get_update_info(
-         '${platform}',
-         '${appVersion}',
-         '${bundleId}',
-         '${minBundleId ?? NIL_UUID}',
-         '${channel}',
-         ARRAY[${targetAppVersionList.map((v) => `'${v}'`).join(",")}]::text[]
-       );
-       `,
-  );
+  }>(sqlGetUpdateInfo, [
+    platform,
+    appVersion,
+    bundleId,
+    minBundleId ?? NIL_UUID,
+    channel,
+    targetAppVersionList,
+  ]);
 
   return result.rows[0] ? (camelcaseKeys(result.rows[0]) as UpdateInfo) : null;
 };
