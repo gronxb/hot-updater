51ed751bfa0f93d9ec208c56ff77d3bc6a5e29e5
gronxb <gron1gh1@gmail.com>
2025-03-29 12:28:57 +0900

    feat(plugin-core): use ts-morph

--- plugins/plugin-core/src/transformTsEnv.ts
+++ plugins/plugin-core/src/transformTsEnv.ts
@@ -1,41 +1,31 @@
-import * as ts from "typescript";
+import { Project, SyntaxKind } from "ts-morph";
 
 export const transformTsEnv = <T extends Record<string, string>>(
   code: string,
   env: T,
 ): string => {
-  const sourceFile = ts.createSourceFile(
-    "temp.ts",
-    code,
-    ts.ScriptTarget.Latest,
-    true,
-  );
+  const project = new Project();
+  const sourceFile = project.createSourceFile("temp.ts", code);
 
-  const transformer = <T extends ts.Node>(
-    context: ts.TransformationContext,
-  ) => {
-    const visitor: ts.Visitor = (node) => {
-      if (ts.isPropertyAccessExpression(node)) {
-        const expression = node.expression;
-        const property = node.name;
-        if (ts.isIdentifier(expression) && expression.text === "HotUpdater") {
-          const key = property.text;
-          const value = env[key];
-          if (value !== undefined) {
-            return ts.factory.createStringLiteral(value);
-          }
-        }
+  sourceFile.forEachDescendant((node) => {
+    if (node.getKind() === SyntaxKind.PropertyAccessExpression) {
+      const propAccess = node.asKind(SyntaxKind.PropertyAccessExpression);
+      if (!propAccess) return;
+
+      const expression = propAccess.getExpression();
+      if (expression.getKind() !== SyntaxKind.Identifier) return;
+
+      const identifier = expression.asKind(SyntaxKind.Identifier);
+      if (!identifier || identifier.getText() !== "HotUpdater") return;
+
+      const propertyName = propAccess.getName();
+      const value = env[propertyName];
+
+      if (value !== undefined) {
+        propAccess.replaceWithText(`"${value}"`);
       }
-      return ts.visitEachChild(node, visitor, context);
-    };
-    return (node: T) => ts.visitNode(node, visitor);
-  };
+    }
+  });
 
-  const result = ts.transform(sourceFile, [transformer as any]);
-  const transformedSourceFile = result.transformed[0] as ts.SourceFile;
-
-  const printer = ts.createPrinter();
-  const transformedCode = printer.printFile(transformedSourceFile);
-
-  return transformedCode;
+  return sourceFile.getFullText();
 };
