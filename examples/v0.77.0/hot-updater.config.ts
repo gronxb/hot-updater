4686a9bb8c81bca57ab209774bff762147aa0998
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-04-17 14:08:36 +0900

    chore: revert to original

---
+++ examples/v0.77.0/hot-updater.config.ts
@@ -0,0 +1,17 @@
+import {metro} from '@hot-updater/metro';
+import {supabaseDatabase, supabaseStorage} from '@hot-updater/supabase';
+import {defineConfig} from 'hot-updater';
+import 'dotenv/config';
+
+export default defineConfig({
+  build: metro({enableHermes: true}),
+  storage: supabaseStorage({
+    supabaseUrl: process.env.HOT_UPDATER_SUPABASE_URL!,
+    supabaseAnonKey: process.env.HOT_UPDATER_SUPABASE_ANON_KEY!,
+    bucketName: process.env.HOT_UPDATER_SUPABASE_BUCKET_NAME!,
+  }),
+  database: supabaseDatabase({
+    supabaseUrl: process.env.HOT_UPDATER_SUPABASE_URL!,
+    supabaseAnonKey: process.env.HOT_UPDATER_SUPABASE_ANON_KEY!,
+  }),
+});
