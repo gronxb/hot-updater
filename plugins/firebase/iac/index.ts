2e4b826a8e82d31aea6326c7a5c0d245fd8bdf6a
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-04-23 01:09:57 +0900

    fix(firebase): edit URL for next step

--- plugins/firebase/iac/index.ts
+++ plugins/firebase/iac/index.ts
@@ -398,7 +398,7 @@ export const runInit = async () => {
 
   p.log.message(
     `Next step: ${link(
-      "https://gronxb.github.io/hot-updater/guide/getting-started/quick-start-with-firebase.html#step-4-add-hotupdater-to-your-project",
+      "https://gronxb.github.io/hot-updater/guide/providers/4_firebase.html#step-4-add-hotupdater-to-your-project",
     )}`,
   );
   p.log.message(
