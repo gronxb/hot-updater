61f5da33b0f777a1fb85a868aff262804b0baee0
gronxb <gron1gh1@gmail.com>
2025-05-01 03:23:26 +0900

    fix(android): supports expo 52

--- packages/react-native/android/src/newarch/ReactIntegrationManager.kt
+++ packages/react-native/android/src/newarch/ReactIntegrationManager.kt
@@ -3,7 +3,9 @@ package com.hotupdater
 import android.content.Context
 import android.util.Log
 import com.facebook.react.ReactApplication
+import com.facebook.react.bridge.JSBundleLoader
 import com.facebook.react.common.LifecycleState
+import java.lang.reflect.Field
 
 class ReactIntegrationManager(
     context: Context,
@@ -25,8 +27,22 @@ class ReactIntegrationManager(
             jsBundleLoaderField.isAccessible = true
             jsBundleLoaderField.set(reactHostDelegate, getJSBundlerLoader(bundleURL))
         } catch (e: Exception) {
+            try {
+                val instanceManager = application.reactNativeHost.reactInstanceManager
+                val bundleLoader: JSBundleLoader? = this.getJSBundlerLoader(bundleURL)
+                val bundleLoaderField: Field =
+                    instanceManager::class.java.getDeclaredField("mBundleLoader")
+                bundleLoaderField.isAccessible = true
+
+                if (bundleLoader != null) {
+                    bundleLoaderField.set(instanceManager, bundleLoader)
+                } else {
+                    bundleLoaderField.set(instanceManager, null)
+                }
+            } catch (e: Exception) {
+                Log.d("HotUpdater", "Failed to setJSBundle (fallback): ${e.message}")
+            }
             Log.d("HotUpdater", "Failed to setJSBundle: ${e.message}")
-            throw IllegalAccessException("Could not setJSBundle")
         }
     }
 
@@ -57,12 +73,10 @@ class ReactIntegrationManager(
                     }
                 } catch (e: Exception) {
                     Log.d("HotUpdater", "Failed to reload: ${e.message}")
-                    throw e
                 }
             }
         } catch (e: Exception) {
             Log.d("HotUpdater", "Failed to reload: ${e.message}")
-            throw e
         }
     }
 }
