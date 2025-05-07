b60c1efb2af744f6067b40e41b8fa8378c44acaf
gronxb <gron1gh1@gmail.com>
2025-05-08 00:47:37 +0900

    fix: monorepo

---
+++ examples/v0.76.1-new-arch/react-native.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const pkg = require('../../packages/react-native/package.json');
+
+module.exports = {
+  project: {
+    ios: {
+      automaticPodsInstallation: true,
+    },
+  },
+  dependencies: {
+    [pkg.name]: {
+      root: path.resolve(__dirname, '..', '..', 'packages', 'react-native'),
+      platforms: {
+        // Codegen script incorrectly fails without this
+        // So we explicitly specify the platforms with empty object
+        ios: {},
+        android: {},
+      },
+    },
+  },
+};
