736227819480418fa6ca26f5e39d3fc03f6dc6e9
gronxb <gron1gh1@gmail.com>
2025-05-11 02:17:11 +0900

    fix: base

--- packages/react-native/android/src/main/java/com/hotupdater/HotUpdaterPackage.kt
+++ packages/react-native/android/src/main/java/com/hotupdater/HotUpdaterPackage.kt
@@ -1,13 +1,13 @@
 package com.hotupdater
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import java.util.HashMap
 
-class HotUpdaterPackage : TurboReactPackage() {
+class HotUpdaterPackage : BaseReactPackage() {
     override fun getModule(
         name: String,
         reactContext: ReactApplicationContext,
@@ -21,7 +21,6 @@ class HotUpdaterPackage : TurboReactPackage() {
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider =
         ReactModuleInfoProvider {
             val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-            val isTurboModule: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
             moduleInfos[HotUpdaterModule.NAME] =
                 ReactModuleInfo(
                     HotUpdaterModule.NAME,
@@ -30,7 +29,6 @@ class HotUpdaterPackage : TurboReactPackage() {
                     false, // needsEagerInit
                     true, // hasConstants
                     false, // isCxxModule
-                    isTurboModule, // isTurboModule
                 )
             moduleInfos
         }
