19163bf4c120dd306798936dcb0a5af0859ce6e3
gronxb <gron1gh1@gmail.com>
2025-05-19 00:09:58 +0900

    fix: firebase

--- plugins/firebase/firebase/functions/getUpdateInfo.ts
+++ plugins/firebase/firebase/functions/getUpdateInfo.ts
@@ -1,5 +1,7 @@
 import type {
+  AppVersionGetBundlesArgs,
   Bundle,
+  FingerprintGetBundlesArgs,
   GetBundlesArgs,
   UpdateInfo,
   UpdateStatus,
@@ -14,6 +16,7 @@ const INIT_BUNDLE_ROLLBACK_UPDATE_INFO: UpdateInfo = {
   shouldForceUpdate: true,
   message: null,
   status: "ROLLBACK",
+  storageUri: null,
 };
 
 const convertToBundle = (data: any): Bundle => ({
@@ -35,9 +38,119 @@ const makeResponse = (bundle: Bundle, status: UpdateStatus): UpdateInfo => ({
   message: bundle.message,
   shouldForceUpdate: status === "ROLLBACK" ? true : bundle.shouldForceUpdate,
   status,
+  storageUri: bundle.storageUri,
 });
 
 export const getUpdateInfo = async (
+  db: Firestore,
+  args: GetBundlesArgs,
+): Promise<UpdateInfo | null> => {
+  switch (args._updateStrategy) {
+    case "appVersion":
+      return appVersionStrategy(db, args);
+    case "fingerprint":
+      return fingerprintStrategy(db, args);
+    default:
+      return null;
+  }
+};
+
+const fingerprintStrategy = async (
+  db: Firestore,
+  {
+    platform,
+    fingerprintHash,
+    bundleId,
+    minBundleId = NIL_UUID,
+    channel = "production",
+  }: FingerprintGetBundlesArgs,
+): Promise<UpdateInfo | null> => {
+  try {
+    let currentBundle: Bundle | null = null;
+    if (bundleId !== NIL_UUID) {
+      const doc = await db.collection("bundles").doc(bundleId).get();
+      if (doc.exists) {
+        const data = doc.data()!;
+        if (data.channel !== channel) {
+          return null;
+        }
+        currentBundle = convertToBundle(data);
+      }
+    }
+
+    if (bundleId.localeCompare(minBundleId) < 0) {
+      return null;
+    }
+
+    const baseQuery = db
+      .collection("bundles")
+      .where("platform", "==", platform)
+      .where("channel", "==", channel)
+      .where("enabled", "==", true)
+      .where("id", ">=", minBundleId)
+      .where("fingerprint_hash", "==", fingerprintHash);
+
+    let updateCandidate: Bundle | null = null;
+    let rollbackCandidate: Bundle | null = null;
+
+    if (bundleId === NIL_UUID) {
+      const snap = await baseQuery.orderBy("id", "desc").limit(1).get();
+      if (!snap.empty) {
+        const data = snap.docs[0].data();
+        updateCandidate = convertToBundle(data);
+      }
+    } else {
+      const updateSnap = await baseQuery
+        .where("id", ">=", bundleId)
+        .orderBy("id", "desc")
+        .limit(1)
+        .get();
+      if (!updateSnap.empty) {
+        const data = updateSnap.docs[0].data();
+        updateCandidate = convertToBundle(data);
+      }
+
+      const rollbackSnap = await baseQuery
+        .where("id", "<", bundleId)
+        .orderBy("id", "desc")
+        .limit(1)
+        .get();
+      if (!rollbackSnap.empty) {
+        const data = rollbackSnap.docs[0].data();
+        rollbackCandidate = convertToBundle(data);
+      }
+    }
+
+    if (bundleId === NIL_UUID) {
+      return updateCandidate ? makeResponse(updateCandidate, "UPDATE") : null;
+    }
+    if (updateCandidate && updateCandidate.id !== bundleId) {
+      return makeResponse(updateCandidate, "UPDATE");
+    }
+
+    if (updateCandidate && updateCandidate.id === bundleId) {
+      if (currentBundle?.enabled) {
+        return null;
+      }
+      return rollbackCandidate
+        ? makeResponse(rollbackCandidate, "ROLLBACK")
+        : INIT_BUNDLE_ROLLBACK_UPDATE_INFO;
+    }
+
+    if (!updateCandidate) {
+      if (rollbackCandidate) {
+        return makeResponse(rollbackCandidate, "ROLLBACK");
+      }
+      return bundleId === minBundleId ? null : INIT_BUNDLE_ROLLBACK_UPDATE_INFO;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error in getUpdateInfo:", error);
+    throw error;
+  }
+};
+
+const appVersionStrategy = async (
   db: Firestore,
   {
     platform,
@@ -45,7 +158,7 @@ export const getUpdateInfo = async (
     bundleId,
     minBundleId = NIL_UUID,
     channel = "production",
-  }: GetBundlesArgs,
+  }: AppVersionGetBundlesArgs,
 ): Promise<UpdateInfo | null> => {
   try {
     let currentBundle: Bundle | null = null;
