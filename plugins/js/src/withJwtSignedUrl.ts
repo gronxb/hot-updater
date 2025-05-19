ccef9620a3a8e4a238254ccac55bd2b65507452c
gronxb <gron1gh1@gmail.com>
2025-05-19 10:30:48 +0900

    fix

--- plugins/js/src/withJwtSignedUrl.ts
+++ plugins/js/src/withJwtSignedUrl.ts
@@ -30,7 +30,7 @@ export const withJwtSignedUrl = async <
   }
 
   const storageUrl = new URL(data.storageUri);
-  const key = `${storageUrl.host}${storageUrl.pathname}`;
+  const key = storageUrl.pathname;
   const token = await signToken(key, jwtSecret);
 
   const url = new URL(reqUrl);
