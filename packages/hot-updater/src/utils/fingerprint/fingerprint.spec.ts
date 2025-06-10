82a27b3e49c9e2bbadfd223cbe6cf451728d5bd6
MJ Studio <mym0404@gmail.com>
2025-06-10 17:40:15 +0900

    Update fingerprint.spec.ts
    Co-authored-by: Sungyu Kang <gron1gh1@gmail.com>

--- packages/hot-updater/src/utils/fingerprint/fingerprint.spec.ts
+++ packages/hot-updater/src/utils/fingerprint/fingerprint.spec.ts
@@ -3,7 +3,7 @@ import os from "os";
 import path from "path";
 import { beforeEach, describe } from "vitest";
 
-const tmpDir = path.resolve(os.tmpdir(), ".hot-updater/test");
+const tmpDir = path.resolve(os.tmpdir(), ".hot-updater“, ”test");
 // const iosDir = path.resolve(tmpDir, "ios");
 
 describe.skip("Fingerprint", () => {
