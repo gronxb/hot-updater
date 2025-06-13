ae41848f646cb97f9d64b4116d38d4de686d2533
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-06-13 12:28:29 +0900

    fix: print error

--- packages/hot-updater/src/commands/fingerprint.ts
+++ packages/hot-updater/src/commands/fingerprint.ts
@@ -12,75 +12,91 @@ import picocolors from "picocolors";
 
 export const handleFingerprint = async () => {
   const s = p.spinner();
-  s.start("Generating fingerprints");
+  try {
+    s.start("Generating fingerprints");
 
-  const fingerPrintRef = await generateFingerprints();
+    const fingerPrintRef = await generateFingerprints();
 
-  s.stop(
-    `Fingerprint generated. iOS: ${fingerPrintRef.ios.hash}, Android: ${fingerPrintRef.android.hash}`,
-  );
-
-  const localFingerprintPath = path.join(getCwd(), "fingerprint.json");
-  if (!fs.existsSync(localFingerprintPath)) {
-    return;
-  }
-
-  const readFingerprint = await fs.promises.readFile(
-    localFingerprintPath,
-    "utf-8",
-  );
-  const localFingerprint = JSON.parse(readFingerprint);
-  if (localFingerprint.ios.hash !== fingerPrintRef.ios?.hash) {
-    p.log.error(
-      "iOS fingerprint mismatch. Please update using 'hot-updater fingerprint create' command.",
+    s.stop(
+      `Fingerprint generated. iOS: ${fingerPrintRef.ios.hash}, Android: ${fingerPrintRef.android.hash}`,
     );
-    process.exit(1);
-  }
 
-  if (localFingerprint.android.hash !== fingerPrintRef.android?.hash) {
-    p.log.error(
-      "Android fingerprint mismatch. Please update using 'hot-updater fingerprint create' command.",
+    const localFingerprintPath = path.join(getCwd(), "fingerprint.json");
+    if (!fs.existsSync(localFingerprintPath)) {
+      return;
+    }
+
+    const readFingerprint = await fs.promises.readFile(
+      localFingerprintPath,
+      "utf-8",
     );
-    process.exit(1);
-  }
+    const localFingerprint = JSON.parse(readFingerprint);
+    if (localFingerprint.ios.hash !== fingerPrintRef.ios?.hash) {
+      p.log.error(
+        "iOS fingerprint mismatch. Please update using 'hot-updater fingerprint create' command.",
+      );
+      process.exit(1);
+    }
 
-  p.log.success("Fingerprint matched");
+    if (localFingerprint.android.hash !== fingerPrintRef.android?.hash) {
+      p.log.error(
+        "Android fingerprint mismatch. Please update using 'hot-updater fingerprint create' command.",
+      );
+      process.exit(1);
+    }
+
+    p.log.success("Fingerprint matched");
+  } catch (e) {
+    if (e instanceof Error) {
+      p.log.error(e.message);
+    }
+    throw e;
+  }
 };
 
 export const handleCreateFingerprint = async () => {
   let diffChanged = false;
 
-  await p.tasks([
-    {
-      title: "Creating fingerprint.json",
-      task: async () => {
-        const localFingerprint = await readLocalFingerprint();
-        const newFingerprint = await createFingerprintJson();
+  try {
+    await p.tasks([
+      {
+        title: "Creating fingerprint.json",
+        task: async () => {
+          const localFingerprint = await readLocalFingerprint();
+          const newFingerprint = await createFingerprintJson();
 
-        if (
-          !localFingerprint ||
-          localFingerprint?.ios?.hash !== newFingerprint.ios.hash ||
-          localFingerprint?.android?.hash !== newFingerprint.android.hash
-        ) {
-          diffChanged = true;
-        }
+          if (
+            !localFingerprint ||
+            localFingerprint?.ios?.hash !== newFingerprint.ios.hash ||
+            localFingerprint?.android?.hash !== newFingerprint.android.hash
+          ) {
+            diffChanged = true;
+          }
 
-        await setFingerprintHash("ios", newFingerprint.ios.hash);
-        await setFingerprintHash("android", newFingerprint.android.hash);
-        return "Created fingerprint.json";
+          await setFingerprintHash("ios", newFingerprint.ios.hash);
+          await setFingerprintHash("android", newFingerprint.android.hash);
+          return "Created fingerprint.json";
+        },
       },
-    },
-  ]);
+    ]);
 
-  if (diffChanged) {
-    p.log.success(
-      picocolors.bold(
-        `${picocolors.blue("fingerprint.json")} has changed, you need to rebuild the native app.`,
-      ),
-    );
-  } else {
-    p.log.success(
-      picocolors.bold(`${picocolors.blue("fingerprint.json")} is up to date.`),
-    );
+    if (diffChanged) {
+      p.log.success(
+        picocolors.bold(
+          `${picocolors.blue("fingerprint.json")} has changed, you need to rebuild the native app.`,
+        ),
+      );
+    } else {
+      p.log.success(
+        picocolors.bold(
+          `${picocolors.blue("fingerprint.json")} is up to date.`,
+        ),
+      );
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      p.log.error(e.message);
+    }
+    throw e;
   }
 };
