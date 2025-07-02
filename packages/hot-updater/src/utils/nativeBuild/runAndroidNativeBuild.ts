a9d21120cf59a96503c88e324015a00fed84198c
MJ Studio <mym0404@gmail.com>
2025-07-02 10:50:04 +0900

    feat: inject minBundleId on android native build

--- packages/hot-updater/src/utils/nativeBuild/runAndroidNativeBuild.ts
+++ packages/hot-updater/src/utils/nativeBuild/runAndroidNativeBuild.ts
@@ -4,6 +4,7 @@ import {
   type RequiredDeep,
   getCwd,
 } from "@hot-updater/plugin-core";
+import { uuidv7 } from "uuidv7";
 import { runGradle } from "./gradle";
 export const runAndroidNativeBuild = async ({
   config,
@@ -12,8 +13,10 @@ export const runAndroidNativeBuild = async ({
 }): Promise<{ buildDirectory: string; outputFile: string }> => {
   const androidProjectPath = path.join(getCwd(), "android");
 
+  const bundleId = uuidv7();
+
   return runGradle({
-    args: {},
+    args: { extraParams: [`-PMIN_BUNDLE_ID=${bundleId}`] },
     appModuleName: config.appModuleName,
     tasks: config.aab
       ? [`bundle${config.variant}`]
