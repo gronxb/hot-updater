03cd02ccb25d93a45784bf3d2cae0b1e40643dde
gronxb <gron1gh1@gmail.com>
2025-08-17 15:19:38 +0900

    fix

--- examples/v0.81.0/babel.config.js
+++ examples/v0.81.0/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'hot-updater/babel-plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        allowlist: ['HOT_UPDATER_SUPABASE_URL'],
+        path: '.env',
+      },
+    ],
+  ],
 };
