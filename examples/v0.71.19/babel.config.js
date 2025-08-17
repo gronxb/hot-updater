44910a86dbab00009711b77bcd00f407da2d02af
gronxb <gron1gh1@gmail.com>
2025-08-17 20:06:29 +0900

    fix

--- examples/v0.71.19/babel.config.js
+++ examples/v0.71.19/babel.config.js
@@ -8,7 +8,7 @@ module.exports = {
         envName: 'APP_ENV',
         moduleName: '@env',
         allowlist: ['HOT_UPDATER_SUPABASE_URL', 'HOT_UPDATER_SENTRY_DSN'],
-        path: '.env',
+        path: '.env.hotupdater',
       },
     ],
   ],
