ae7701f7214a27d3ae1aa008dc8ba53d18d92fe8
gronxb <gron1gh1@gmail.com>
2025-08-17 17:37:14 +0900

    fix

--- packages/react-native/android/src/oldarch/ReactIntegrationManager.kt
+++ packages/react-native/android/src/oldarch/ReactIntegrationManager.kt
@@ -1,39 +1,33 @@
 package com.hotupdater
 
+import android.content.Context
 import android.util.Log
 import com.facebook.react.ReactApplication
 import com.facebook.react.bridge.JSBundleLoader
-import com.facebook.react.bridge.ReactApplicationContext
 import java.lang.reflect.Field
 
 class ReactIntegrationManager(
-    reactContext: ReactApplicationContext,
-) : ReactIntegrationManagerBase(reactContext) {
+    context: Context,
+) : ReactIntegrationManagerBase(context) {
     public fun setJSBundle(
         application: ReactApplication,
         bundleURL: String,
     ) {
-        val reactHost = application.reactHost
-        // println("ReactIntegrationManager.setJSBundle: $bundleURL")
-        Log.d("ReactIntegrationManager", "ReactIntegrationManager.setJSBundle: $bundleURL")
-        Log.d("ReactIntegrationManager", "ReactIntegrationManager.reactHost: $reactHost")
+        try {
+            val instanceManager = application.reactNativeHost.reactInstanceManager
+            val bundleLoader: JSBundleLoader? = this.getJSBundlerLoader(bundleURL)
+            val bundleLoaderField: Field =
+                instanceManager::class.java.getDeclaredField("mBundleLoader")
+            bundleLoaderField.isAccessible = true
 
-        reactHost
-            .try {
-                val instanceManager = application.reactNativeHost.reactInstanceManager
-                val bundleLoader: JSBundleLoader? = this.getJSBundlerLoader(bundleURL, bundleURL)
-                val bundleLoaderField: Field =
-                    instanceManager::class.java.getDeclaredField("mBundleLoader")
-                bundleLoaderField.isAccessible = true
-
-                if (bundleLoader != null) {
-                    bundleLoaderField.set(instanceManager, bundleLoader)
-                } else {
-                    bundleLoaderField.set(instanceManager, null)
-                }
-            } catch (e: Exception) {
-                Log.d("HotUpdater", "Failed to setJSBundle: ${e.message}")
+            if (bundleLoader != null) {
+                bundleLoaderField.set(instanceManager, bundleLoader)
+            } else {
+                bundleLoaderField.set(instanceManager, null)
             }
+        } catch (e: Exception) {
+            Log.d("HotUpdater", "Failed to setJSBundle: ${e.message}")
+        }
     }
 
     /**
