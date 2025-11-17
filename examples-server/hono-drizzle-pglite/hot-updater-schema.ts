637f92a42f79d56647fcce2fd400133afab4daeb
gronxb <gron1gh1@gmail.com>
2025-11-17 20:22:44 +0900

    fix

---
+++ examples-server/hono-drizzle-pglite/hot-updater-schema.ts
@@ -0,0 +1,31 @@
+import {
+  boolean,
+  json,
+  pgTable,
+  text,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const bundles = pgTable("bundles", {
+  id: uuid("id").primaryKey().notNull(),
+  platform: text("platform").notNull(),
+  should_force_update: boolean("should_force_update").notNull(),
+  enabled: boolean("enabled").notNull(),
+  file_hash: text("file_hash").notNull(),
+  git_commit_hash: text("git_commit_hash"),
+  message: text("message"),
+  channel: text("channel").notNull(),
+  storage_uri: text("storage_uri").notNull(),
+  target_app_version: text("target_app_version"),
+  fingerprint_hash: text("fingerprint_hash"),
+  metadata: json("metadata").notNull(),
+});
+
+export const private_hot_updater_settings = pgTable(
+  "private_hot_updater_settings",
+  {
+    id: varchar("id", { length: 255 }).primaryKey().notNull(),
+    version: varchar("version", { length: 255 }).notNull().default("0.21.0"),
+  },
+);
