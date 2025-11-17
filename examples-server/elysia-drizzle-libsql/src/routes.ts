9c15182e1a79798949c40ae0ac2f63405200c574
gronxb <gron1gh1@gmail.com>
2025-11-17 19:36:33 +0900

    fix

--- examples-server/elysia-drizzle-libsql/src/routes.ts
+++ examples-server/elysia-drizzle-libsql/src/routes.ts
@@ -1,11 +1,11 @@
 import { Elysia } from "elysia";
-import { api } from "./db.js";
+import { hotUpdater } from "./db.js";
 
 const app = new Elysia();
 
 // Mount Hot Updater handler for all /api/* routes
 app.all("/api/*", ({ request }) => {
-  return api.handler(request);
+  return hotUpdater.handler(request);
 });
 
 export default app;
