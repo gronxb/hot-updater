1b839b461ca0729b6da6d581fa32992ded80988e
MJ Studio <mym0404@gmail.com>
2025-11-24 16:00:58 +0900

    chore: unconscious commit

--- packages/apple-helper/src/utils/prettifyXcodebuildError.ts
+++ packages/apple-helper/src/utils/prettifyXcodebuildError.ts
@@ -9,10 +9,15 @@ export const prettifyXcodebuildError = (error: any) => {
   ) {
     errorString = error.message;
   }
-  if (errorString.includes("error: No profiles for")) {
+
+  if (
+    errorString.includes("error: No profiles for") ||
+    errorString.includes("error: Provisioning profile")
+  ) {
     return new Error(
       "Xcodebuild failed: Not valid provisioning profile is set in the project. Check out Singing & Capabilities tab in the Xcode.",
     );
   }
+
   return new Error(`Xcodebuild failed: ${error}`);
 };
