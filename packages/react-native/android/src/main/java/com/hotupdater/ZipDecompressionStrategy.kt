ba066e9488c869301e83bf6e7fd2139a3fa1ba90
gronxb <gron1gh1@gmail.com>
2025-12-09 23:43:46 +0900

    fix

--- packages/react-native/android/src/main/java/com/hotupdater/ZipDecompressionStrategy.kt
+++ packages/react-native/android/src/main/java/com/hotupdater/ZipDecompressionStrategy.kt
@@ -5,6 +5,7 @@ import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
 import java.util.zip.CRC32
 import java.util.zip.ZipEntry
 import java.util.zip.ZipException
