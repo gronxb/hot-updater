a64d9e75eef2f1699fed0386383a33ae239127f0
gronxb <gron1gh1@gmail.com>
2026-03-20 16:33:43 +0900

    chore: remove repack plugin package

--- plugins/repack/src/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Compiler, RspackPluginInstance } from "@rspack/core";
-
-export class HotUpdaterPlugin implements RspackPluginInstance {
-  apply(_compiler: Compiler) {
-    // Bundle IDs are now issued at deploy/build-plugin time and stored in manifest.json.
-  }
-}
