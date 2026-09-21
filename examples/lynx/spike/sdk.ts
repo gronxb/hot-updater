a6d7486f6d4706288dbf4c865e40378458f50ccd
gronxb <gron1gh1@gmail.com>
2026-09-22 08:07:56 +0900

    fix(lynx): use runtime text codec for evidence

--- examples/lynx/spike/sdk.ts
+++ examples/lynx/spike/sdk.ts
@@ -8,6 +8,9 @@ declare const __SPIKE_VARIANT__: string;
 declare const __SPIKE_BEHAVIOR__: string;
 declare const __SPIKE_ASSET_PREFIX__: string;
 declare const __SDK_RESOURCES__: boolean;
+declare const TextCodecHelper: {
+  encode(value: string): ArrayBuffer;
+};
 
 export const variant = __SPIKE_VARIANT__;
 export const resources = __SDK_RESOURCES__;
@@ -20,32 +23,8 @@ let busy = false;
 let ready = false;
 let evidenceOrigin: string | null = null;
 
-const utf8 = (value: string) => {
-  const bytes: number[] = [];
-  for (const character of value) {
-    const codePoint = character.codePointAt(0)!;
-    if (codePoint <= 0x7f) bytes.push(codePoint);
-    else if (codePoint <= 0x7ff) {
-      bytes.push(0xc0 | (codePoint >> 6), 0x80 | (codePoint & 0x3f));
-    } else if (codePoint <= 0xffff) {
-      bytes.push(
-        0xe0 | (codePoint >> 12),
-        0x80 | ((codePoint >> 6) & 0x3f),
-        0x80 | (codePoint & 0x3f),
-      );
-    } else {
-      bytes.push(
-        0xf0 | (codePoint >> 18),
-        0x80 | ((codePoint >> 12) & 0x3f),
-        0x80 | ((codePoint >> 6) & 0x3f),
-        0x80 | (codePoint & 0x3f),
-      );
-    }
-  }
-  return Uint8Array.from(bytes);
-};
-
-const jsonBody = (value: unknown) => utf8(JSON.stringify(value));
+const jsonBody = (value: unknown) =>
+  TextCodecHelper.encode(JSON.stringify(value));
 
 export function sdkImageLoaded() {
   imageReady = true;
